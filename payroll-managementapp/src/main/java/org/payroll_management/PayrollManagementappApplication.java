73b3a2ab384388094bf8a28732a778b3b9768533
saisrinivasreddysangana <saireddy4java@gmail.com>
2025-04-25 17:32:45 +0530

    Add files via upload

---
+++ payroll-managementapp/src/main/java/org/payroll_management/PayrollManagementappApplication.java
@@ -0,0 +1,13 @@
+package org.payroll_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollManagementappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollManagementappApplication.class, args);
+	}
+
+}
---
+++ payroll-managementapp/src/test/java/org/payroll_management/PayrollManagementappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.payroll_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollManagementappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
